3de855a2849e7ca7959cd88b3bd0357df5a85284
Bhasker-github <bbasker037@gmail.com>
2023-09-03 22:52:57 +0530

    done

--- hlo.java
+++ hlo.java
@@ -1,5 +1,20 @@
+package hello;
+import java.sql.*;
+
 public class hlo{
-  public static void main(String[] args) {
-    
+  public static void main(String[] args)throws Exception {
+    String url="jdbc:mysql://localhost:3306/lion";
+    String uname="root";
+    String pass="mysql";
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection con=DriverManager.getConnection(url, uname, pass);
+    Statement st=con.createStatement();
+    ResultSet rs=st.executeQuery("select *from lb");
+    while(rs.next()){
+        String a=rs.getInt("sno")+" "+rs.getString("name");
+System.out.println(a);
+    }
+   con.close();
+   st.close();
   }
 }
